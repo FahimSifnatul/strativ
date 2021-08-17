d8366a74b6eb2e42c762cfb6b7f8ee1cbb4dfd32
FahimSifnatul <sifnatul2475@gmail.com>
2021-08-17 23:14:12 +0600

    Added functionality to search bar - Phase 3 completed

--- home/views.py
+++ home/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from rest_framework.views import APIView
+from rest_framework.parsers import JSONParser
+from urllib.request import urlopen, Request
 
 # custom
 from strativ_api.models import Countries
@@ -12,13 +15,25 @@ class Home(APIView):
 		return render(request, 'home.html', context)
 
 	def post(self, request, *args, **kwargs):
-		country_name = list(request.POST.items())[0][0] # To retrieve country name from Form name
-		country = Countries.objects.get(name=country_name)
+		if 'search_country_text' in request.POST:
+			name = request.POST['search_country_text']
+			http_response_object = urlopen('http://127.0.0.1:8000/search-country/'+name)
+			
+			country = JSONParser().parse(http_response_object)
+			if len(country) == 0:
+				return HttpResponse("<h1>No country exists with the name '" + name + "'</h1>")
 
-		context = {}
-		context['name'] = country_name
-		context['flag'] = country.flag
-		context['borders'] = country.borders
-		context['languages'] = country.languages
-		
-		return render(request, 'details.html', context)
+			context = {}
+			context['countries'] = country
+			return render(request, 'home.html', context)
+		else:
+			country_name = list(request.POST.items())[0][0] # To retrieve country name from Form name
+			country = Countries.objects.get(name=country_name)
+
+			context = {}
+			context['name'] = country_name
+			context['flag'] = country.flag
+			context['borders'] = country.borders
+			context['languages'] = country.languages
+			
+			return render(request, 'details.html', context)
