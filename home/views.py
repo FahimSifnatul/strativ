cbb9eeb19a2ef6f99e1ceb1d3b3e4fb261206528
FahimSifnatul <sifnatul2475@gmail.com>
2021-08-17 19:22:48 +0600

    Added country summary table

--- home/views.py
+++ home/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 
+# custom
+from strativ_api.models import Countries
+
 # Create your views here.
 class Home(APIView):
 	def get(self, request, *args, **kwargs):
 		context = {}
+		context['countries'] = Countries.objects.all()
 		return render(request, 'home.html', context)
