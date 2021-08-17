e14326d894d727c477a8c82fa7cf59493672caac
FahimSifnatul <sifnatul2475@gmail.com>
2021-08-17 08:52:01 +0600

    Retrieved API data from restcountries.eu/rest/v2/all

--- strativ_api/views.py
+++ strativ_api/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
+from urllib.request import urlopen
+
+# custom 
+from .models import Countries
 
 # Create your views here.
 class CollectAPI(APIView):
 	def get(self, request, *args, **kwargs):
-		return Response(request.GET)
+		url = 'https://restcountries.eu/rest/v2/all'
+		countries = JSONParser().parse(urlopen(url)) 
+
+		return Response(countries)
