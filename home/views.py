4b7b0f53d2bf39232dba02545209084f720eef2b
FahimSifnatul <sifnatul2475@gmail.com>
2021-08-17 21:42:25 +0600

    Short message Added details.html and functionality to details button
    Long message
    Added details.html template to display details of a specific country and also added functionality to details button to act accordingly

--- home/views.py
+++ home/views.py
@@ -10,3 +10,15 @@ class Home(APIView):
 		context = {}
 		context['countries'] = Countries.objects.all()
 		return render(request, 'home.html', context)
+
+	def post(self, request, *args, **kwargs):
+		country_name = list(request.POST.items())[0][0] # To retrieve country name from Form name
+		country = Countries.objects.get(name=country_name)
+
+		context = {}
+		context['name'] = country_name
+		context['flag'] = country.flag
+		context['borders'] = country.borders
+		context['languages'] = country.languages
+		
+		return render(request, 'details.html', context)
