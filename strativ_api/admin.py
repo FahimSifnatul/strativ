2f9872a416251d021ef479f2096465cc6f469cdc
FahimSifnatul <sifnatul2475@gmail.com>
2021-08-17 07:35:03 +0600

    Created superuser and model

--- strativ_api/admin.py
+++ strativ_api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+# custom models
+from .models import Countries
 
 # Register your models here.
+admin.site.register(Countries)
--- strativ_api/models.py
+++ strativ_api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Countries(models.Model):
+	name       = models.CharField(max_length=32)
+	alpha2code = models.CharField(max_length=16)
+	capital    = models.CharField(max_length=32)
+	population = models.IntegerField()
+	timezones  = models.CharField(max_length=16)
+	flag       = models.CharField(max_length=64)
+	languages  = models.CharField(max_length=256)
+	borders    = models.CharField(max_length=256)
