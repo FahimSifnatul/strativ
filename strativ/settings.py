3ad8a75022994fec17f69c27775f237f6cb36d12
FahimSifnatul <sifnatul2475@gmail.com>
2021-08-19 03:07:18 +0600

    Set DEBUG=False as everything seems OK

--- strativ/settings.py
+++ strativ/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3q+h1*dlyg%s@8zdd_k7rvhlhklpr04ni_(fr54+ls67hd1yd$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['strativ-assignment.herokuapp.com', 'http://127.0.0.1:8000/', '.localhost', '[::1]']
 
